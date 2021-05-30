a950973503c4f49d7a84bef1571893d229088cb1
vFISHv <vfishv@gmail.com>
2021-05-30 11:47:36 +0800

    import

--- app/src/main/java/brut/util/Jar.java
+++ app/src/main/java/brut/util/Jar.java
@@ -23,6 +23,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ThreadLocalRandom;
 
 import org.apache.commons.io.IOUtils;
 
